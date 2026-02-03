9882cf38bdce6fa99eb938d32a647b2292d0143b
pinkara <tomamattle@gmail.com>
2026-02-03 21:06:15 +0100

    Big commit

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MathUnivers/',
+  base: process.env.NODE_ENV === 'production' ? '/MathUnivers/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
